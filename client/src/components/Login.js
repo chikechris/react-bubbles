dbe57e2a9cb05e4f53185cff12384fe07e9f8937
chikechris <cchike2017@gmail.com>
2019-08-23 11:56:31 -0400

    building out Login functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React from 'react'
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <h2>User Name</h2>
+        <input
+          type='text'
+          name='username'
+          value={input.username}
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          name='password'
+          value={input.username}
+          onChange={handleChange}
+        />
+        <div>
+          <Button> Login </Button>
+        </div>
+      </form>
     </>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
