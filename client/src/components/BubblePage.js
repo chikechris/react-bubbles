70fa0aeb4cf4f912b4ba02c46e9989e41eb07e75
chikechris <cchike2017@gmail.com>
2019-08-25 04:01:39 -0400

    created seperate file for FormStyles

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -4,21 +4,7 @@ import Bubbles from './Bubbles';
 import ColorList from './ColorList';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-
-const Button = styled.button`
-  button {
-    background: #282c34;
-    text-align: center;
-    padding: 8px;
-    margin-top: 10px;
-    border-radius: 30px;
-    color: white;
-    cursor: pointer;
-    text-transform: uppercase;
-    margin-bottom: 10px;
-  }
-`;
+import Button from './FormStyle';
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,79 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import FormStyle from './FormStyle';
 
-const FormStyle = styled.div`
-  body {
-    width: 100%;
-    height: 100%;
-    margin-top: 10%;
-    transform: translateY(-20%);
-    position: absolute;
-    display: flex;
-    align-items: center;
-    align-content: center;
-    justify-content: center;
-  }
-  form {
-    display: flex;
-    max-width: 50%;
-    width: 100%;
-    height: 100%;
-    margin: 0 auto;
-    flex-direction: column;
-    align-items: center;
-    align-content: center;
-    justify-content: center;
-    padding: 30px;
-    border-radius: 5px;
-  }
-
-  input {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    margin-right: 4%;
-    font-family: 'Serif ', 'Georgia ';
-    margin: 5px 0;
-    background: transparent;
-    border: 0px;
-    border-bottom: 2px solid #282c34;
-    padding: 10px;
-    color: 'white';
-    width: 80%;
-  }
-  button {
-    background: #282c34;
-    text-align: center;
-    padding: 5px;
-    margin-top: 10px;
-    border-radius: 30px;
-    color: white;
-    cursor: pointer;
-    text-transform: uppercase;
-    margin-bottom: 10px;
-  }
-  p {
-    text-align: center;
-    color: blue;
-  }
-`;
-
-function Login({ errors, touched, status, props }) {
+function Login({ errors, touched }) {
   return (
     <FormStyle>
-      <div className='form-module'>
-        <Form className='loginForm'>
+      <div>
+        <Form>
           {touched.username && errors.username && <p>{errors.username}</p>}
-          <Field
-            className='zr_un_email valid'
-            type='username'
-            name='username'
-            placeholder='username'
-          />
+          <Field type='username' name='username' placeholder='username' />
 
           {touched.password && errors.password && <p>{errors.password}</p>}
           <Field type='password' name='password' placeholder='Password' />
